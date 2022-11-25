f77b921e883837a294c83df63766a382dbc73632
Tamara <99070482+thetamaraelaye@users.noreply.github.com>
2022-11-25 12:39:38 +0100

    updated script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "navbar") {
+      x.className += "responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
+  
